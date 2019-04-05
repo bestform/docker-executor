eefdfa3b8ca220b0b0d70592c6c9b3af80151046
Matthias Derer <matthias.derer@esono.de>
2019-04-05 13:14:46 +0200

    add port to config and fix html output

--- internal/conf.go
+++ internal/conf.go
@@ -3,11 +3,13 @@ package internal
 import (
 	"github.com/pkg/errors"
 	"os"
+	"strconv"
 )
 
 type Config struct {
 	Cmd string
 	Identifier string
+	Port int
 }
 
 func ConfigFromEnvironment() (Config, error) {
@@ -22,6 +24,16 @@ func ConfigFromEnvironment() (Config, error) {
 	c.Identifier, ok = os.LookupEnv("DOCKER_EXECUTOR_IDENTIFIER")
 	if !ok {
 		return c, errors.New("DOCKER_EXECUTOR_IDENTIFIER must be defined in the environment")
+	}
+	port, ok := os.LookupEnv("DOCKER_EXECUTOR_PORT")
+	if !ok {
+		port = "8080"
+	}
+
+	var err error
+	c.Port, err = strconv.Atoi(port)
+	if err != nil {
+		return c, errors.Wrap(err, "Could not parse port")
 	}
 
 	return c, nil
--- main.go
+++ main.go
@@ -7,54 +7,68 @@ import (
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
 	"io/ioutil"
-	"os"
+	"net/http"
+	"strconv"
 	"strings"
 )
+
+var config internal.Config
 
 func main() {
 	var err error
 
+	config, err = internal.ConfigFromEnvironment()
+	if err != nil { return }
+
+	http.HandleFunc("/", handleRequest)
+
+	if err = http.ListenAndServe(":" + strconv.Itoa(config.Port), nil); err != nil {
+		return
+	}
+}
+
+func handleRequest(writer http.ResponseWriter, request *http.Request) {
+	var err error
 	defer func() {
 		if err != nil {
 			fmt.Println("Error running client:", err)
-			os.Exit(1)
+			writer.WriteHeader(500)
 		}
 	}()
 
-	config, err := internal.ConfigFromEnvironment()
-	if err != nil { return }
+	writer.Header().Add("Content-Type", "text/html")
 
 	cli, err := client.NewEnvClient()
 	if err != nil { return }
 
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil { return }
-
 	for _, c := range containers {
 		if isTargetContainerByName(c.Names, config) {
-			err = executeCommand(cli, c, config)
+			output, err := executeCommand(cli, c, config)
+			if err != nil { return }
+			_, err = fmt.Fprint(writer, string(output))
 			if err != nil { return }
 		}
 	}
-
 }
 
-func executeCommand(cli *client.Client, c types.Container, config internal.Config) error {
-	fmt.Println("Executing on container", c.ID)
+func executeCommand(cli *client.Client, c types.Container, config internal.Config) ([]byte, error) {
+	var err error
+	var output []byte
+
 	cmdParts := strings.Split(config.Cmd, " ")
 	idResp, err := cli.ContainerExecCreate(context.Background(), c.ID, types.ExecConfig{Cmd:cmdParts, AttachStdout:true, AttachStderr:true})
-	if err != nil { return err }
+	if err != nil { return output, err }
 
 	hjResp, err := cli.ContainerExecAttach(context.Background(), idResp.ID, types.ExecConfig{})
-	if err != nil { return err }
+	if err != nil { return output, err }
 	defer hjResp.Close()
 
-	output, err := ioutil.ReadAll(hjResp.Reader)
-	if err != nil { return err }
+	output, err = ioutil.ReadAll(hjResp.Reader)
+	if err != nil { return output, err }
 
-	fmt.Println(string(output))
-
-	return nil
+	return output, nil
 }
 
 func isTargetContainerByName(names []string, config internal.Config) bool {
